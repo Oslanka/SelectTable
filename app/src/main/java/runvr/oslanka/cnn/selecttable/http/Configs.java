9431daa831b30ad3e049c2d85b6f021f1efbae50
Oslanka <Oslana@163.com>
2018-04-19 10:18:54 +0800

    430k

--- app/src/main/java/runvr/oslanka/cnn/selecttable/http/Configs.java
+++ app/src/main/java/runvr/oslanka/cnn/selecttable/http/Configs.java
@@ -5,7 +5,8 @@ package runvr.oslanka.cnn.selecttable.http;
  */
 
 public class Configs {
-    public static final String baseUrl="http://119.23.221.98:8080/SelectWeb/";
+//    public static final String baseUrl="http://119.23.221.98:8080/SelectWeb/";
+    public static final String baseUrl="http://121.42.225.128:8080/SelectWeb/";
 //    public static final String baseUrl="http://192.168.1.106:8080/SelectWeb/";
 //    public static final String baseUrl="http://10.1.130.36:8080/SelectWeb/";
 
